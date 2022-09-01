d88c46b0c5b8edeb51987f889dc495b36207eddb
CummingsLoganT <109982415+CummingsLoganT@users.noreply.github.com>
2022-09-01 13:58:45 -0500

    potential bug fix

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+    rollbar.log("Access HTML successfully");
+})
+
+
 const port = process.env.PORT || 5050
 
 app.listen(port, () => console.log(`Server listening on ${port}`))
